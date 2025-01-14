65ed980ca7033b0d9c7f01a923b1f424f909eab5
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-14 16:54:08 +0100

    Logical: template literals

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ let a= false;
 let b= true;
 
 console.log( a && getName("Alice jack"));
+
+
+
+// Template literals
+
+let name1 = "John"
+let name2 = "Doe"
+
+console.log(name1 + " " + name2, `${name1} ${name2}`);
